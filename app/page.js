4f4361404c34182721656b60d73592cc97a4949e
Anil Bridgpal <bridgpal>
2024-05-01 19:29:15 -0400

    installed

--- app/page.js
+++ app/page.js
@@ -1,9 +1,25 @@
-import Image from "next/image";
+'use client';
 
+import Image from "next/image";
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch, SearchBox, Hits } from 'react-instantsearch';
+
+const searchClient = algoliasearch(
+  'latency',
+  '6be0576ff61c053d5f9a3225e2a90f76'
+);
+
+console.log(searchClient);
 export default function Home() {
   return (
-    <main>
-      <h1>Algolia Test</h1>
+    <main className="p-10">
+      <h1 className="large">Algolia Test</h1>
+
+      <InstantSearch indexName="bestbuy" searchClient={searchClient}>
+    <SearchBox />
+    <Hits />
+  </InstantSearch>
+
     </main>
   );
 }
